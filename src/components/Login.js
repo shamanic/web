949cc099a5e2e9b09329468c95b3e562f754355f
Audry <audrywolters@gmail.com>
2019-09-25 14:05:43 -0500

    apparently it was up to date code

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,60 +1,74 @@
 import React, {Component} from 'react';
 
-export default class Login extends Component {
+class Login extends Component {
     constructor(props) {
-        super(props)
+        super(props);
 
         this.state = {
             username: '',
             password: ''
-        }
+        };
+
+        // AUDRY this seems really old school...
+        // no hooks cuz is class?
+        // https://reactjs.org/docs/hooks-state.html
+        // make it a const Login = () => ...
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onClickLogin     = this.onClickLogin.bind(this);
+    };
+
+    // AUDRY - again, these seem clunky...
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        })
     }
 
-    handleChangeUsername = e => {
-        this.setState({ username: e.target.value })
+    onChangePassword(e) {
+        this.setState({
+            password: e.target.value
+        })
     }
 
-    handleChangePassword = e => {
-        this.setState({ password: e.target.value })
-    }
-
-    handleClickLogin = e => {
+    onClickLogin(e) {
         // AUDRY null/garbage validators
 
         e.preventDefault();
-        console.log('hey ' + this.state.username + '. yr secret: ' + this.state.password);
+        console.log('hey ' + this.state.username);
 
         // AUDRY - route to DB. if get bad response - tell user
         // if get OK, init user and route to game page
         // looks like David's route is / or /index :)
-        // Need help w/ production / Heroku
+        // Need help w/ production code and Heroku
     }
 
     render () {
         return (
             <div className="floating">
                 <h3>Login (and watch your console)</h3>
-                {/* de-form (ha ha) this? what is pro/con? */}
-                <form onSubmit={this.handleClickLogin}>
+                <form onSubmit={this.onClickLogin}>
                     <label>Username: </label>
-                    {/* DAVID: what username length did you set in DB? */}
+                    {/* length ok? */}
                     <input type="text"
                            maxLength="25"
                            value={this.state.username}
-                           onChange={this.handleChangeUsername}
+                           onChange={this.onChangeUsername}
                     />
 
                     <label>Password: </label>
-                    {/* DAVID: what password length did you set in DB? */}
+                    {/* length ok? */}
                     <input type="password"
                            maxLength="60"
                            value={this.state.password}
-                           onChange={this.handleChangePassword}
+                           onChange={this.onChangePassword}
                     />
                     <input type="submit" value="Login" />
                 </form>
-                <button>Create New Account</button>
             </div>
         )
     }
 }
+
+export default Login;
